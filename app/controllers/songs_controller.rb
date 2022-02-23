95103698472469bbfa5e852c4a6c29535ffb4d89
github username <github email address>
2022-02-22 21:32:43 -0600

    commit tree f99db9c550ba6f6adc4e7e3c8c1e5d817370201e

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+    end
+
+    def new
+        @song = Song.new
+    end
+
+    def show
+        @song = Song.find(params[:id])
+    end
+
+    def create
+        @song = Song.new(params.require(:song).permit(:name))
+        @song.save
+        redirect_to song_path(@song)
+    end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def update
+        @song = Song.update(params.require(:song).permit(:name))
+        redirect_to song_path(@song)
+    end
+end
